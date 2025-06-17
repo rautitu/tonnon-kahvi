b87e38ed64375c0ffd70089212477f52e21e6ae4
rautitu <tuomas.rautiola@gmail.com>
2025-06-17 19:16:13 +0300

    added a check to verify postgres is up and running before inserts start

--- fetcher/fetcher_main.py
+++ fetcher/fetcher_main.py
@@ -1,8 +1,32 @@
+import time
+import os
+import psycopg2
+
 from fetcher.fetchers.kesko_fetcher import KRuokaFetcher
 from unit_tests import test_postgres_existence
 
+def wait_for_postgres(max_retries=10, delay=2):
+    for i in range(max_retries):
+        try:
+            conn = psycopg2.connect(
+                host=os.environ['DB_HOST'],
+                database=os.environ['DB_NAME'],
+                user=os.environ['DB_USER'],
+                password=os.environ['DB_PASSWORD']
+            )
+            conn.close()
+            print("✅ Connected to PostgreSQL!")
+            return
+        except psycopg2.OperationalError as e:
+            print(f"❌ Attempt {i + 1}: PostgreSQL not ready yet... Retrying in {delay}s")
+            time.sleep(delay)
+    raise Exception("❌ PostgreSQL connection failed after retries.")
+
 def orchestrate_price_fetch_and_insert():
     """Runs fetch_and_insert of every fetcher we have (defined in the list)"""
+    #check if postgress is up and running
+    wait_for_postgres(max_retries=10, delay=2)
+    
     fetchers: list = [KRuokaFetcher()]
     all_results: list[str] = []
     for fetcher in fetchers:
