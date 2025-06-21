c83a3eac87fb3fcbbfe16e5580fd304f9c3549cd
Tuomas Rautiola <ext-tuomas.rautiola@insta.fi>
2025-06-21 15:48:58 +0300

    adding some test method for the backend API

--- backend/main.py
+++ backend/main.py
@@ -4,3 +4,7 @@ from routers import get_coffee_prices
 app = FastAPI(title="Coffee API")
 
 app.include_router(get_coffee_prices.router)
+
+@app.get("/")
+def root():
+    return {"message": "Hello, Coffee API!"}
