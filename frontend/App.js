a7e9d13c76e9c181e24317d5dc8e86c8fec4f8b2
rautitu <tuomas.rautiola@gmail.com>
2025-06-28 11:20:26 +0300

    adding app.js with using test method to my backend

---
+++ frontend/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text } from 'react-native';
+import axios from 'axios';
+
+export default function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/coffees')
+      .then((res) => setData(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <View>
+      <Text>Data from backend: {JSON.stringify(data)}</Text>
+    </View>
+  );
+}
