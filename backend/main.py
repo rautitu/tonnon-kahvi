e3304b792f6f1d3a0a30bd9bdda1e4aab09404bf
rautitu <tuomas.rautiola@gmail.com>
2025-06-28 10:39:50 +0300

    attempting a fix again

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers.get_coffee_prices import get_coffee_prices
+from routers.get_coffee_prices import router
 #from routers import get_coffee_prices
 
 app = FastAPI(title="Coffee API")
