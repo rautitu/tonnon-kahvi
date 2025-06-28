2dce2762fcca76303f01d31fe4b6786d8142aaba
rautitu <tuomas.rautiola@gmail.com>
2025-06-28 10:53:04 +0300

    testing listing all methods from backend through the API

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
 from routers.get_coffee_prices import router
-#from routers import get_coffee_prices
+from fastapi.routing import APIRoute
 
 app = FastAPI(title="Coffee API")
 
-#app.include_router(get_coffee_prices.router)
 app.include_router(router)
 
-@app.get("/")
+@app.get("/hello")
 def root():
     return {"message": "Hello, Coffee API!"}
+
+@app.get("/")
+def get_all_endpoints():
+    routes = []
+    for route in app.routes:
+        if isinstance(route, APIRoute):
+            routes.append({
+                "path": route.path,
+                "name": route.name,
+                "methods": list(route.methods)
+            })
+    return {
+        "message": "TonnoCoffeeAPI available endpoints:",
+        "available_endpoints": routes
+    }
