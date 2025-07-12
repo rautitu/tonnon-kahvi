e8e68fd0cb7966fe1cc4070be4946bc34e6f182a
rautitu <tuomas.rautiola@gmail.com>
2025-07-12 13:06:09 +0300

    testing again

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,38 +1,39 @@
-import { View, Text } from 'react-native';
-
-export default function App() {
-  return (
-    <View>
-      <Text>Hello Web!</Text>
-    </View>
-  );
-}
-
-//import React from 'react';
-//import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-//import { View, Text, StyleSheet } from 'react-native-web'; 
-//import Home from './pages/Home';
-//import Endpoints from './pages/Endpoints';
-//import Coffees from './pages/Coffees';
+//import { View, Text } from 'react-native';
 //
 //export default function App() {
 //  return (
-//    <Router>
-//      <View style={styles.container}>
-//        <Routes>
-//          <Route path="/" element={<Home />} />
-//          <Route path="/endpoints" element={<Endpoints />} />
-//          <Route path="/coffees" element={<Coffees />} />
-//        </Routes>
-//      </View>
-//    </Router>
+//    <View>
+//      <Text>Hello Web!</Text>
+//    </View>
 //  );
 //}
-//
-//const styles = StyleSheet.create({
-//  container: {
-//    flex: 1,
-//    padding: 24,
-//    backgroundColor: '#fff',
-//  },
-//});
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { View, Text, StyleSheet } from 'react-native-web'; 
+import Home from './pages/Home';
+import Endpoints from './pages/Endpoints';
+import Coffees from './pages/Coffees';
+
+export default function App() {
+  return (
+    <Router>
+      <View style={styles.container}>
+        <Text>Available subpages:</Text>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/endpoints" element={<Endpoints />} />
+          <Route path="/coffees" element={<Coffees />} />
+        </Routes>
+      </View>
+    </Router>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+});
